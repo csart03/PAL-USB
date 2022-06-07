36847ea2b675900db3dd8057302d5f8e99b0e33f
csart03 <arthraj9@gmail.com>
2022-06-07 13:53:47 +0530

    setsymlinks.sh fixed

--- installations/setsymlinks.sh
+++ installations/setsymlinks.sh
@@ -1,10 +1,8 @@
-cd /usr/local/lib
-sudo cp $OLDPWD/../lib/libPAL_PAL.so /usr/local/lib
-sudo cp $OLDPWD/../lib/libPAL_CAMERA_PAL.so /usr/local/lib
-sudo cp $OLDPWD/../lib/libPAL_DATA_PAL.so /usr/local/lib
-sudo cp $OLDPWD/../lib/libPAL_DE_PAL.so /usr/local/lib
-sudo cp $OLDPWD/../lib/libPAL_DEPTH_128_PAL.so /usr/local/lib
-sudo cp $OLDPWD/../lib/libPAL_DEPTH_HQ_PAL.so /usr/local/lib
-sudo cp $OLDPWD/../lib/libPAL_EDET_PAL.so /usr/local/lib
-sudo cp $OLDPWD/../lib/libPAL_Track_PAL.so /usr/local/lib
-cd -
+sudo cp ../lib/libPAL_PAL.so /usr/local/lib
+sudo cp ../lib/libPAL_CAMERA_PAL.so /usr/local/lib
+sudo cp ../lib/libPAL_DATA_PAL.so /usr/local/lib
+sudo cp ../lib/libPAL_DE_PAL.so /usr/local/lib
+sudo cp ../lib/libPAL_DEPTH_128_PAL.so /usr/local/lib
+sudo cp ../lib/libPAL_DEPTH_HQ_PAL.so /usr/local/lib
+sudo cp ../lib/libPAL_EDET_PAL.so /usr/local/lib
+sudo cp ../lib/libPAL_Track_PAL.so /usr/local/lib
